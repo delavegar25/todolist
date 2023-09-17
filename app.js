ecf6b045fa18bcf81af5e391bd95e29ec9fa665c
Pascal <pascalokereke18@gmail.com>
2023-09-17 17:23:54 +0100

    confused.

---
+++ app.js
@@ -0,0 +1,8 @@
+const myTodo = document.getElementById('todo');
+const saveButton = document.getElementsByClassName('Save');
+const removeButton = document.getElementsByClassName('remove');
+
+// console.log(myTodo);
+// console.log(saveBtn);
+// console.log(removeBtn);
+
