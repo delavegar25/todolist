66b4713f20855f35a618ab4ce050955fe5e13e9e
Pascal <pascalokereke18@gmail.com>
2023-09-21 15:28:44 +0100

    not sure

--- app.js
+++ app.js
@@ -1,18 +1,27 @@
-const myTodo = document.getElementById('todo');
-const saveButton = document.querySelector('.Save');
-const removeButton = document.querySelector('.remove');
+const saveBtn = document.querySelector('.Save');
+const removeBtn = document.querySelector('.delete');
 
-// console.log(myTodo);
-// console.log(saveButton);
-// console.log(removeButton);
-
-saveButton.addEventListener('click', () => {
-    saveButton.classList.add('Save');
-});
-removeButton.addEventListener('click', () => {
-    removeButton.classList.remove('delete');
+removeBtn.addEventListener('click', (e) => {
+    if(e.target.className === 'delete'){
+        const div = e.target.parentElement;
+        li.parentNode.removeChild(div);
+    }
 });
 
-function saveBtn() {
+saveBtn.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+// create element
+const saveButton = document.createElement('div');
+const removeButton = document.createElement('div');
 
-}
+// add text content
+saveButton.textContent = 'add';
+removeButton.textContent = 'delete'
+
+saveButton.classList.add('Save');
+removeButton.classList.add('delete');
+
+div.appendChild(saveButton);
+div.appendChild(removeButton);
+});
